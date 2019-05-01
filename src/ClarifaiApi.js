fefe6566ee3905076c9cc8fc7e4f09db9daa9f5b
Jennifer Enck <jenniferenck@mac.com>
2019-05-01 09:56:38 -0700

    added dev notes for using clarifai api

--- src/ClarifaiApi.js
+++ src/ClarifaiApi.js
@@ -1,4 +1,16 @@
 const Clarifai = require('clarifai');
 import CLARIFAI_API from '../env';
 
+// https://clarifai.com/developer/guide/
+
+/* we will need to use the "predict" API 
+
+    INPUTS --> image, what model to use (models contain a group of concepts)
+        ** Models are helpful since they act as params for what to look for
+        ** You can specify mor params for prediction results
+
+    OUTPUT --> list of concepts contained within image
+
+ * */
+
 const app = new Clarifai.App({ apiKey: CLARIFAI_API });
