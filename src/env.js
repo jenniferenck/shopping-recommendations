433a3e327660d699f0dc2455062f53cd13ebe4e2
Jennifer Enck <jenniferenck@mac.com>
2019-05-01 09:27:41 -0700

    added api key holder to ignore

--- src/env.js
+++
@@ -1,8 +0,0 @@
-// You will need to paste a working key below from GIPHY API into the string below:
-// Steps:
-//  Go to: https://developers.giphy.com/
-//  Select `Create an App`, and create an account to generate a key
-
-const GIPHY_KEY = 'yourAPIkeygoeshere';
-
-export default GIPHY_KEY;
