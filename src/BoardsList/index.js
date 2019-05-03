6cad0826d5421727218252a4766dc3c2542d4ee4
Jennifer Enck <jenniferenck@mac.com>
2019-05-03 14:27:22 -0700

    added board container component

---
+++ src/BoardsList/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class BoardsList extends Component {
+  render() {
+    return <div>A list of boards goes here...</div>;
+  }
+}
+
+export default BoardsList;
