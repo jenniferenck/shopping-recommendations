c7c4d665f8bc00bbdf39df55ae476e667e5540c0
Jennifer Enck <jenniferenck@mac.com>
2019-05-06 11:20:27 -0700

    added centering for boardlist

--- src/BoardsList/index.js
+++ src/BoardsList/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import HorizontalList from '../HorizontalList';
+import './BoardList.css';
 
 class BoardsList extends Component {
   render() {
     return (
-      <div>
+      <div className="board-list">
         <HorizontalList boards={this.props.boards} />
       </div>
     );
