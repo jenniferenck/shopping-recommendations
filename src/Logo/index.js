595dbc19f0aa5b3b5f8b64f1c4dbb49e1c3b7b1c
Jennifer Enck <jenniferenck@mac.com>
2019-05-06 12:19:52 -0700

    added text spacing aesthetics

--- src/Logo/index.js
+++ src/Logo/index.js
@@ -5,7 +5,7 @@ class Logo extends Component {
   render() {
     return (
       <div className="logo-container">
-        <h2 id="logo-abbreviation">MN</h2>
+        <h2 id="logo-abbreviation">M|N</h2>
         <p id="logo-small-text">Mine.Now.</p>
       </div>
     );
