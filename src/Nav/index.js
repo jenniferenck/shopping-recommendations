81ad60cdbbb0fae1edcd14fa793e92fc4658f194
Jennifer Enck <jenniferenck@mac.com>
2019-05-09 11:09:00 -0700

    adjusted nav to flexbox and contents

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 import './Nav.css';
 import Button from '../Button';
@@ -6,6 +7,14 @@ import Logo from '../Logo';
 
 class Nav extends Component {
   render() {
+    const categories = [
+      'pair of jeans',
+      'party dress',
+      'necklace',
+      'jacket',
+      'arm chair',
+      'birthday present'
+    ];
     const { allowedAccess, activeSearch, handleActiveSearch } = this.props;
     return (
       <div className="nav">
@@ -22,15 +31,27 @@ class Nav extends Component {
             <a href="http://">Account</a>
           </li>
         </ul>
-
-        <div className="intro-text-section">
-          <div className="intro-text">Been searching for that perfect {}?</div>
-          <div className="intro-text">Look no further</div>
-          <div className="intro-text">
-            Select up to 5 pins to start a search, or start a custom search with
-            your own criteria
+        <CSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={1000}
+          transitionEnter={false}
+          transitionLeave={false}
+          key="example"
+        >
+          <div className="intro-text-section">
+            <div className="intro-text">
+              Been searching for that perfect {}?
+            </div>
+            <div className="intro-text">Look no further.</div>
+            <div className="intro-text">
+              Select up to 5 pins to start a search,
+            </div>
+            <div className="intro-text">
+              or start a custom search with your own criteria
+            </div>
           </div>
-        </div>
+        </CSSTransitionGroup>
 
         {activeSearch ? (
           <Button onClick={handleActiveSearch} />
