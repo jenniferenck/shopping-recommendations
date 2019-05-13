5eb549954e20a61f6f4e485f235ac454d39f7a29
Jennifer Enck <jenniferenck@mac.com>
2019-05-13 09:36:44 -0700

    added initial styles

--- src/WelcomePage/index.js
+++ src/WelcomePage/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import './WelcomePage.css';
 
 class WelcomePage extends Component {
   render() {
     return (
-      <div>
-        Display this page if the user has not yet shared their Pins
-        <div>Left side with button and text</div>
-        <div>Right side with Images Plain</div>
+      <div className="welcome-page">
+        Logo up at top
+        <div className="welcome-page-body">
+          <div className="left-side">Left side with button and text</div>
+          <div className="right-side">Right side with Images Plain</div>
+        </div>
       </div>
     );
   }
