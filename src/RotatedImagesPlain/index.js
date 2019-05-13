cfd23fda53fbfdf9b2b1da9163335d9db7ceceeb
Jennifer Enck <jenniferenck@mac.com>
2019-05-13 09:29:31 -0700

    added logged out page components

---
+++ src/RotatedImagesPlain/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ImageItem from '../ImageItem';
+
+class RotatedImagesPlain extends Component {
+  render() {
+    return (
+      <div>
+        <ImageItem />
+        <ImageItem />
+        <ImageItem />
+        <ImageItem />
+      </div>
+    );
+  }
+}
+
+export default RotatedImagesPlain;
---
+++ src/WelcomePage/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class WelcomePage extends Component {
+  render() {
+    return (
+      <div>
+        Display this page if the user has not yet shared their Pins
+        <div>Left side with button and text</div>
+        <div>Right side with Images Plain</div>
+      </div>
+    );
+  }
+}
+
+export default WelcomePage;
