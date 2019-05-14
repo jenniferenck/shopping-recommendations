ea114d2b25db31d494d542c738a91fc7c9eac831
Jennifer Enck <jenniferenck@mac.com>
2019-05-13 21:51:43 -0700

    cleaned up code by removing scroll event

--- src/App/index.js
+++ src/App/index.js
@@ -81,57 +81,6 @@ class App extends Component {
     window.removeEventListener('scroll', this.onScroll);
   }
 
-  //  Replace this with 'fetchBoards'
-
-  // API request for gifs that meet search term
-  // async fetchGifs(searchTerm) {
-  //   this.setState({ activeSearch: true });
-  //   const searchResults = await GiphyApi.fetchGifs(searchTerm);
-  //   this.setState({
-  //     recentSearchGifs: searchResults.data,
-  //     currentSearchTerm: searchTerm
-  //   });
-  // }
-
-  onScroll = () => {
-    const {
-      fetchMoreGifs,
-      state: {
-        loadingMoreGifs
-        // hasMore,
-      }
-    } = this;
-
-    // Bails early if:
-    // * there's an error
-    // * it's already loading
-    // * there's nothing left to load
-    // || !hasMore (for later implementation)
-    if (loadingMoreGifs) return;
-
-    // Checks that the page has scrolled to the bottom
-    if (
-      window.innerHeight + document.documentElement.scrollTop ===
-      document.documentElement.offsetHeight
-    ) {
-      // fetchMoreGifs(this.state.offset);
-    }
-  };
-
-  // example below handles pagination and infinite scroll
-
-  // fetches more gifs with current offset and adds to trending/ recentsearch
-  // async fetchMoreGifs() {
-  //   this.setState({ loadingMoreGifs: true });
-  //   const moreGifs = await GiphyApi.fetchMoreGifs(this.state.offset);
-  //   // update offset increment by 25 and add new gifs to list
-  //   this.setState(st => ({
-  //     offset: st.offset + 25,
-  //     trendingGifs: [...st.trendingGifs, ...moreGifs],
-  //     loadingMoreGifs: false
-  //   }));
-  // }
-
   toggleSearchBarView = () => {
     this.setState(st => ({ activeSearch: !st.activeSearch }));
   };
